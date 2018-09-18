cacc580d615852d06643bd5a508ece05c15c3cb4
BT_1N3_05@BKTECH.LOCAL <BT_1N3_05@BKTECH.LOCAL>
2018-09-18 08:44:36 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str = "Good morning kings lets get this bread";
+        System.out.println("input = " + str);
+        String str2 = "I love this class ";
+        String str3 = "but why is it first period";
+        System.out.print(str2);
+        System.out.println(str3);
+        // I could've added the two strings into one line but I wanted to test out just print
+    }
+}
